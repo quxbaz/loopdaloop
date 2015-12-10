4a548899ff88c95ca782dc58c9b51bd310b1c891
David Yeung <quxbaz@gmail.com>
2015-12-09 20:56:34 -0500

    Test change to api.

--- ldl/api/__init__.py
+++ ldl/api/__init__.py
@@ -9,3 +9,12 @@ rest = Api(blueprint)
 @blueprint.route('/')
 def app():
   return render_template('home.html')
+
+class Comments(Resource):
+  def get(self):
+    return [
+      {'id': 1, 'author': "Pete Hunt", 'text': "This is one comment"},
+      {'id': 2, 'author': "Shabba Doo", 'text': "This is *another* comment"}
+    ]
+
+rest.add_resource(Comments, '/api/comments')
