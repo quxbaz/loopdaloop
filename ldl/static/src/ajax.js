1e33355fcfd22e0b001c114fe483719927b7428f
David Yeung <quxbaz@gmail.com>
2015-12-15 16:05:49 -0500

    ajax.get now accepts options argument.

--- ldl/static/src/ajax.js
+++ ldl/static/src/ajax.js
@@ -1,16 +1,16 @@
 module.exports = {
-  get: function(url, responseType) {
+  get: function(url, opts) {
+    if (typeof options == 'undefined')
+      var opts = {};
     var request = new XMLHttpRequest();
     request.open('GET', url, true);
-    if (typeof responseType !=  'undefined')
-      request.responseType = responseType;
+    if (opts.hasOwnProperty('responseType'))
+      request.responseType = opts.responseType;
     var promise = new Promise(function(resolve, reject) {
       request.onload = function() {
         resolve(request.response);
       };
-      request.onerror = function() {
-        console.error('GET error:', request.response);
-      };
+      request.onerror = console.error.bind(console, 'GET error:', request.response);
     });
     request.send();
     return promise;
