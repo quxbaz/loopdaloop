a0140ab04299328f8d95359bf67538191caf6766
David Yeung <quxbaz@gmail.com>
2015-12-14 21:23:51 -0500

    Blip now uses defaults and accepts volume opts.

--- ldl/static/src/audio/blip.js
+++ ldl/static/src/audio/blip.js
@@ -1,15 +1,22 @@
+var _ = require('underscore');
 var audioManager = require('./audiomanager');
 
 function Blip(opts) {
   /*
     A single sound.
   */
-  this.audio = audioManager.clone(opts.src);
-  this.duration = opts.duration || 200;
+  var defaults = {
+    sample: 'hihat',
+    duration: 200,
+    volume: 1
+  };
+  _.extend(this, defaults, opts);
+  this.audioNode = audioManager.clone(this.sample);
+  this.audioNode.volume = this.volume;
 }
 
 Blip.prototype.play = function() {
-  this.audio.play();
+  this.audioNode.play();
   return this;
 };
 
