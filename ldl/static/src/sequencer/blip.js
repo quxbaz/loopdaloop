f78270904a10a689744a80d8684a72e407c8d1e7
David Yeung <quxbaz@gmail.com>
2015-12-16 21:07:07 -0500

    blip now plays webaudio objects.

--- ldl/static/src/sequencer/blip.js
+++ ldl/static/src/sequencer/blip.js
@@ -1,22 +1,24 @@
 var _ = require('underscore');
-var audioManager = require('./audiomanager');
 
-function Blip(opts) {
+function Blip(context, opts) {
   /*
     A single sound.
   */
+  this.context = context;
   var defaults = {
     sample: 'hihat',
     duration: 200,
     volume: 1
   };
   _.extend(this, defaults, opts);
-  this.audioNode = audioManager.clone(this.sample);
   this.audioNode.volume = this.volume;
 }
 
 Blip.prototype.play = function() {
-  this.audioNode.play();
+  var source = context.createBufferSource();
+  source.buffer = this.context.getSampleBuffer(this.sample);
+  source.connect(context.destination);
+  source.start();
   return this;
 };
 
