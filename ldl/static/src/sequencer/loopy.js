7456f2616d1359f1a9322a9550deaf2e75af3254
David Yeung <quxbaz@gmail.com>
2015-12-17 15:04:37 -0500

    loopy.Loop is now regular javascript prototype instead of backbone model.

--- ldl/static/src/sequencer/loopy.js
+++ ldl/static/src/sequencer/loopy.js
@@ -3,52 +3,69 @@
 */
 
 var _ = require('underscore');
+var $ = require('jquery');
 var Backbone = require('backbone');
+var hb = require('handlebars');
 var Blip = require('./blip');
 
-var Loop = Backbone.Model.extend({
+function Loop(opts) {
+  var defaults = {
+    repeat: true
+  };
+  var opts = _.extend(defaults, opts) || defaults;
+  this.exists = true;
+  this.blips = [];
+  this.repeat = opts.repeat;
+}
 
-  initialize: function(opts) {
-    var defaults = {
-      repeat: true
-    };
-    var opts = _.extend(defaults, opts) || defaults;
-    this.blips = [];
-    this.repeat = opts.repeat;
-  },
+var fn = Loop.prototype;
 
-  add: function(sampleId, opts) {
-    var blip = new Blip(sampleId, opts);
-    this.blips.push(blip);
-    return this;
-  },
+fn.add = function(sampleId, opts) {
+  var blip = new Blip(sampleId, opts);
+  this.blips.push(blip);
+  return this;
+};
 
-  play: function() {
-    var durations = _.pluck(this.blips, 'duration');
-    var totalDuration = _.reduce(durations, function(a, b) {return a + b;});
-    var durationMap = _.initial(durations);
-    if (durationMap.length > 1) {
-      for (var i=1; i < durationMap.length; i++)
-        durationMap[i] += durationMap[i-1];
-    }
-    for (var i=0; i < this.blips.length; i++) {
-      if (i == 0)
-        this.blips[i].play();
-      else {
-        (function(blip) {
-          setTimeout(function() {
-            blip.play();
-          }, durationMap[i-1]);
-        })(this.blips[i]);
-      }
-    }
-    if (this.repeat)
-      setTimeout(this.play.bind(this), totalDuration);
+fn.play = function() {
+  var durations = _.pluck(this.blips, 'duration');
+  var totalDuration = _.reduce(durations, function(a, b) {return a + b;});
+  var durationMap = _.initial(durations);
+  if (durationMap.length > 1) {
+    for (var i=1; i < durationMap.length; i++)
+      durationMap[i] += durationMap[i-1];
   }
+  for (var i=0; i < this.blips.length; i++) {
+    if (i == 0)
+      this.blips[i].play();
+    else {
+      (function(blip) {
+        setTimeout(function() {
+          blip.play();
+        }, durationMap[i-1]);
+      })(this.blips[i]);
+    }
+  }
+  if (this.repeat)
+    setTimeout(this.play.bind(this), totalDuration);
+};
 
+var LoopView = Backbone.View.extend({
+  className: 'loop',
+  template: hb.compile($('#loop-template').html()),
+  events: {
+    'click': 'onClick'
+  },
+  initialize: function(loop) {
+    this.loop = loop;
+  },
+  onClick: function(event) {
+    this.$el.trigger('select-loop', this.loop);
+  },
+  render: function() {
+    this.$el.html(this.template());
+    return this;
+  }
 });
-
-var LoopView = Backbone.View.extend({});
 
 module.exports = {
   Loop: Loop,
