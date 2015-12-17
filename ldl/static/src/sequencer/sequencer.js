53c4022a416b708baf78290857774494e88222b7
David Yeung <quxbaz@gmail.com>
2015-12-17 11:25:17 -0500

    Removed bunch of test crud.

--- ldl/static/src/sequencer/sequencer.js
+++ ldl/static/src/sequencer/sequencer.js
@@ -3,17 +3,7 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 var hb = require('handlebars');
 var Array2d = require('arr2d');
-var audioManager = require('./audiomanager');
 var Loop = require('./loop');
-
-var LoopView = Backbone.View.extend({
-  className: 'loop',
-  template: hb.compile($('#loop-template').html()),
-  render: function() {
-    this.$el.html(this.template(this.model))
-    return this;
-  }
-});
 
 var Sequencer = Backbone.View.extend({
 
@@ -22,8 +12,6 @@ var Sequencer = Backbone.View.extend({
 
   events: {
     'click .blip-btn'     : 'addBlip',
-    'click .add-loop'     : 'addLoop',
-    'click .loop'         : 'selectLoop',
     'click .control-play' : 'play'
   },
 
@@ -34,47 +22,29 @@ var Sequencer = Backbone.View.extend({
     this.grid = new Array2d(8, 10);
 
     // TEMP
-    this.loops = [];
+    this.testLoop = new Loop({repeat: false});
   },
 
   addBlip: function(event) {
-    var sample = $(event.currentTarget).data('sample');
-    var duration = 300;
-    this.activeLoop.add({sample: sample, duration: duration});
+    var sampleId = $(event.currentTarget).data('sample-id');
+    console.log(sampleId);
+    var duration = 200;
+    this.testLoop.add(sampleId, {
+      duration: duration
+    });
     this.render(); // TEMP
-    return this;
-  },
-
-  addLoop: function() {
-    this.loops.push(new Loop());
-    if (this.loops.length == 1)
-      this.activeLoop = this.loops[0];
-    this.render(); // TEMP
-    return this;
-  },
-
-  selectLoop: function(event) {
-    $('.loop').removeClass('outline');
-    var $loop = $(event.currentTarget);
-    $loop.addClass('outline');
-    this.activeLoop = _.findWhere(this.loops, {cid: $loop.data('cid')});
     return this;
   },
 
   play: function() {
-    for (var i=0; i < this.loops.length; i++)
-      this.loops[i].play();
+    this.testLoop.play();
   },
 
   render: function() {
     this.$el.html(this.template({
-      samples: _.keys(audioManager.samples)
+      sampleIds: app.am.sampleIds,
+      loop: this.testLoop
     }));
-    for (var i=0; i < this.loops.length; i++) {
-      var view = new LoopView({model: this.loops[i]});
-      view.render().$el.appendTo($('.loops', this.el));
-      view.$el.attr('data-cid', this.loops[i].cid);
-    }
     return this;
   }
 
