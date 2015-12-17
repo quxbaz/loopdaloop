d8d61de1c3072c1b327ab7852aafd23639494994
David Yeung <quxbaz@gmail.com>
2015-12-17 10:52:49 -0500

    App now sets global 'name' to itself on instantiation.

--- ldl/static/src/app.js
+++ ldl/static/src/app.js
@@ -3,6 +3,7 @@ var Sequencer = require('./sequencer/sequencer');
 var AudioManager = require('./sequencer/audiomanager');
 
 function App() {
+  window.app = this;            // GLOBAL
   this.am = new AudioManager(new AudioContext());
   this.sequencer = new Sequencer();
 }
