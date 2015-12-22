e3e6c5eca4246a6e73cc772e43271ecf829771cd
David Yeung <quxbaz@gmail.com>
2015-12-21 20:24:29 -0500

    Added add-channel functionality.

--- ldl/static/src/sequencer/sequencer.js
+++ ldl/static/src/sequencer/sequencer.js
@@ -1,9 +1,8 @@
 var $ = require('jquery');
+var _ = require('underscore');
 var Backbone = require('backbone');
 var util = require('util');
 var Channel = require('./channel');
-
-var INITIAL_CHANNELS = 3;  // Starting number of channels rendered
 
 var Sequencer = Backbone.View.extend({
 
@@ -11,13 +10,47 @@ var Sequencer = Backbone.View.extend({
   template: util.makeTemplate('sequencer'),
 
   events: {
-    'click .control-play' : 'play',
-    'click .blip-btn'     : 'addBlip'
+    'click .add-channel': 'actionAddChannel'
   },
 
   initialize: function() {
     this.channels = [];
-    this.addChannels(INITIAL_CHANNELS);
+    this.addChannels(1);
+    this.currentBeat = 0;
+    this.beatDuration = 200;
+
+    this.testInitChannels();
+    // _.delay(this.play.bind(this), 100);
+  },
+
+  testInitChannels: function() {
+    var Blip = require('./blip');
+    var samples = [
+      [
+        'kick', 'clap', 'snare', 'clap',
+        'kick', 'clap', 'snare', 'clap',
+        'kick', 'clap', 'snare', 'clap',
+        'kick', 'hihat', 'hihat', 'snare'
+      ],
+      // [
+      //   'kick' , '', 'kick', 'kick',
+      //   ''     , '', ''    , ''    ,
+      //   'kick' , '', 'kick', ''    ,
+      //   ''     , '', ''    , ''
+      // ],
+      // [
+      //   '', '', 'hihat', 'hihat',
+      //   '', '', '', '',
+      //   '', '', '', '',
+      //   'kick', 'clap', 'kick', 'clap'
+      // ]
+    ];
+    for (var i=0; i < samples.length; i++) {
+      for (var n=0; n < 16; n++) {
+        // this.channels[i].loop.add(new Blip(samples[i][n % 4], {duration: this.beatDuration}));
+        this.channels[i].loop.add(new Blip(samples[i][n], {duration: this.beatDuration}));
+      }
+    }
   },
 
   addChannels: function(n) {
@@ -25,16 +58,35 @@ var Sequencer = Backbone.View.extend({
       this.channels.push(new Channel());
   },
 
-  play: function() {
+  actionAddChannel: function(event) {
+    this.addChannels(1);
+    this.render();
   },
 
-  addBlip: function(event) {
-    if (!this.selectedLoop)
-      return;
-    var sampleId = $(event.currentTarget).data('sample-id');
-    this.selectedLoop.add(sampleId);
-    this.render();
-    return this;
+  play: function() {
+
+    var that = this;
+    setInterval(function() {
+      $('.cell').removeClass('playing');
+      $('.cells').each(function(i) {
+
+        // <todo> Play random blips :D
+        // that.currentBeat = Math.floor(Math.random() * 16);
+
+        if (that.currentBeat == 0) {
+          that.channels[i].play();
+        }
+
+        $('.cell', this)[that.currentBeat].className += ' playing';
+
+      });
+      that.currentBeat = (that.currentBeat + 1) % 16;
+    }, this.beatDuration);
+
+    // for (var i=0; i < this.channels.length; i++) {
+    //   this.channels[i].play();
+    // }
+
   },
 
   templateData: function() {
